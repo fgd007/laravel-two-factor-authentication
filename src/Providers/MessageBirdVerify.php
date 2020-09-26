700fa8bdb952d30e34e0ae8e1d358e3e46c943ff
floris83 <florisdouma@gmail.com>
2020-09-26 20:39:33 +0200

    typo

--- src/Providers/MessageBirdVerify.php
+++ src/Providers/MessageBirdVerify.php
@@ -113,7 +113,7 @@ class MessageBirdVerify extends BaseProvider implements TwoFactorProvider, SMSTo
         $verify = new Verify;
         $verify->recipient = $user->getMobile();
 
-        $userOptions = ['type' => 'sms', 'locale' => $user->getLocale()];
+        $userOptions = ['type' => 'sms', 'language' => $user->getLocale() != 'nl' : 'en' : 'nl'];
         if (is_null($verify->recipient)) {
             $verify->recipient = $user->getPhonenumber();
             $userOptions['type'] = 'tts';
